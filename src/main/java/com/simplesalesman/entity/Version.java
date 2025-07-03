8ff814cf4c2215a5c454c50c69abe716b5d1c44b
Yaro-bit <piraterieforyou@proton.me>
2025-07-03 09:14:51 +0200

    initialise course test

---
+++ src/main/java/com/simplesalesman/entity/Version.java
@@ -0,0 +1,52 @@
+// 1. Entity Class (Version.java)
+package com.simplesalesman.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "t_version")
+public class Version {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    @NotBlank(message = "Version cannot be empty")
+    @Size(max = 255, message = "Version cannot exceed 255 characters")
+    private String version;
+    
+    // Default constructor
+    public Version() {}
+    
+    // Constructor
+    public Version(String version) {
+        this.version = version;
+    }
+    
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getVersion() {
+        return version;
+    }
+    
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    @Override
+    public String toString() {
+        return "Version{" +
+                "id=" + id +
+                ", version='" + version + '\'' +
+                '}';
+    }
+}
